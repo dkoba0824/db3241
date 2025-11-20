c9bbb04312330f0b7f8a1485a3066ad276b3ee8a
Armita Fard <armitafard@Armitas-MBP.lan>
2025-11-19 22:07:38 -0500

    added report menu 1 and 6

--- equipmentRental/src/menus/ReportsMenu.java
+++ equipmentRental/src/menus/ReportsMenu.java
@@ -8,9 +8,12 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import database.Database;
+import entities.Equipment;
+
 import java.util.Scanner;
 
 public class ReportsMenu {
+    private static final Scanner scanner = new Scanner(System.in);
 
     // Reports Menu will be implemented here in future checkpoint
     public static void reportsMenu() {
@@ -42,38 +45,79 @@ public class ReportsMenu {
         } while (choice != 0);
     }
     private static void rentalCheckouts() {
-        System.out.println("Rental Checkouts");
+        System.out.println("Rental Checkouts: Find total items rented by a member.");
+
+        System.out.print("Enter member ID: ");
+        String memberId = scanner.nextLine(); 
+
+        String sql = "SELECT COUNT(*) AS Total_Rented FROM Rental WHERE User_ID = ?";
+
+
+        Database.psTotalItemsRented(sql, memberId);
     }
     private static void popularItem() {
-        System.out.println("Popular Item");
+        System.out.println("Most Popular Item");
+        String sql = """
+        SELECT E.SerialNo, E.Model, COUNT(H.SerialNo) AS Total_Rented 
+        FROM Equipment E, Has_Rental AS H 
+        WHERE E.SerialNo = H.SerialNo 
+        GROUP BY E.SerialNo 
+        ORDER BY Total_Rented DESC
+        LIMIT 1;
+    """;
+       
+    Database.runQuery(sql);
+
+        
     }
     private static void popularManufacturer() {
         System.out.println("Most Popular Manufacturer:");
         String sql = "SELECT M.Name AS Manufacturer, COUNT(H.SerialNo) AS Total_Rented FROM Manufacturer AS M, Equipment AS E, Has_Rental AS H WHERE M.M_ID = E.M_ID AND E.SerialNo = H.SerialNo GROUP BY M.M_ID ORDER BY Total_Rented DESC LIMIT 1;";
 
-        try (Connection conn = Database.getConnection();
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ResultSet rs = ps.executeQuery()){  
+        Database.runQuery(sql);
 
-            if (rs.next()) {
-                String manufacturer = rs.getString("Manufacturer");
-                int total = rs.getInt("Total_Rented");
-                System.out.println(manufacturer + " - " + total);
-            }
-            rs.close();
-            ps.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
     }
     private static void popularDrone() {
-        System.out.println("Popular Drone");
+        System.out.println("Most Popular Drone");
+        String sql = """
+        SELECT D.SerialNo, D.Name, SUM(C.Ware_Dist) AS Dist_Traveled, 
+               COUNT(DD.SerialNo) AS Total_Deliveries
+        FROM Drones AS D
+        JOIN Drone_Delivers AS DD ON D.SerialNo = DD.D_Serial
+        JOIN Equipment AS E ON E.SerialNo = DD.SerialNo
+        JOIN Has_Rental AS H ON E.SerialNo = H.SerialNo
+        JOIN Customers AS C ON C.User_ID = H.User_ID
+        GROUP BY D.SerialNo
+        ORDER BY Total_Deliveries DESC
+        LIMIT 1;
+    """;
+    Database.runQuery(sql);
+        
     }
     private static void ItemsOut() {
-        System.out.println("Items Checked Out");
-    }
+        System.out.println("Member with the most checkouts:");
+        String sql = "SELECT User_ID, COUNT(SerialNo) AS Rental_Amount " +
+                 "FROM Has_Rental " +
+                 "GROUP BY User_ID " +
+                 "ORDER BY Rental_Amount DESC " +
+                 "LIMIT 1;";
+                 Database.runQuery(sql);
+
+        }
+    
     private static void EquipByType() {
-        System.out.println("Equipment by Type");
+        System.out.println("Equipment by Type: Find equipment of a certain type released before a given year.");
+
+        System.out.print("Enter equipment type: ");
+        String type = scanner.nextLine(); 
+        System.out.print("Enter year: ");
+        int year = Utilities.getIntInput();
+
+        String sql = "SELECT Descrip " +
+        "FROM Equipment " +
+        "WHERE Type = ? AND Year < ?;";
+
+        Database.psEquipmentByTypeBeforeYear(sql, type, year);
     }
 
 }
