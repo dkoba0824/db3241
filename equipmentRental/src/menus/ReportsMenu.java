3b4f198d240dff895495b9a3038ddf1e6ca09d32
Armita Fard <armitafard@Armitas-MBP.lan>
2025-11-19 20:03:40 -0500

    added report menu 3

--- equipmentRental/src/menus/ReportsMenu.java
+++ equipmentRental/src/menus/ReportsMenu.java
@@ -1,7 +1,13 @@
 package menus;
 
 import utilities.Utilities;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
+import database.Database;
 import java.util.Scanner;
 
 public class ReportsMenu {
@@ -23,16 +29,52 @@ public class ReportsMenu {
             choice = Utilities.getIntInput();
 
             switch (choice) {
-                case 1 -> System.out.println("[Report] Total items rented.");
-                case 2 -> System.out.println("[Report] Most popular item.");
-                case 3 -> System.out.println("[Report] Most popular manufacturer.");
-                case 4 -> System.out.println("[Report] Most used drone.");
-                case 5 -> System.out.println("[Report] Member with most checkouts.");
-                case 6 -> System.out.println("[Report] Equipment by type before year.");
+                case 1 -> rentalCheckouts();
+                case 2 -> popularItem();
+                case 3 -> popularManufacturer();
+                case 4 -> popularDrone();
+                case 5 -> ItemsOut() ;
+                case 6 -> EquipByType();
                 case 0 -> System.out.println("Returning to main menu...");
                 default -> System.out.println("Invalid choice.");
             }
 
         } while (choice != 0);
     }
+    private static void rentalCheckouts() {
+        System.out.println("Rental Checkouts");
+    }
+    private static void popularItem() {
+        System.out.println("Popular Item");
+    }
+    private static void popularManufacturer() {
+        System.out.println("Most Popular Manufacturer:");
+        String sql = "SELECT M.Name AS Manufacturer, COUNT(H.SerialNo) AS Total_Rented FROM Manufacturer AS M, Equipment AS E, Has_Rental AS H WHERE M.M_ID = E.M_ID AND E.SerialNo = H.SerialNo GROUP BY M.M_ID ORDER BY Total_Rented DESC LIMIT 1;";
+
+        try (Connection conn = Database.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery()){  
+
+            if (rs.next()) {
+                String manufacturer = rs.getString("Manufacturer");
+                int total = rs.getInt("Total_Rented");
+                System.out.println(manufacturer + " - " + total);
+            }
+            rs.close();
+            ps.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    private static void popularDrone() {
+        System.out.println("Popular Drone");
+    }
+    private static void ItemsOut() {
+        System.out.println("Items Checked Out");
+    }
+    private static void EquipByType() {
+        System.out.println("Equipment by Type");
+    }
+
 }
+
